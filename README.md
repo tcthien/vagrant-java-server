746dfa44bed572b16f974958ab8b8d3c19377f4c
Thien Tran <thientran1986@gmail.com>
2016-12-23 23:06:30 +0700

    Add vagrant file

---
+++ README.md
@@ -0,0 +1,186 @@
+Creates server development environment for java developer
+
+* Download JDK directly
+```
+    #java8
+    wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-linux-x64.tar.gz
+
+    #java7
+    wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.tar.gz
+```
+
+* mysql (starts on boot)
+  * all character sets set to ```utf8``` 
+  * server 
+    * management (```Usage: /etc/init.d/mysql start|stop|restart|reload|force-reload|status```)
+      * ```sudo service mysql stop```
+      * ```sudo service mysql start```
+      * ```sudo service mysql restart```
+      * ```sudo service mysql reload```
+      * ```sudo service mysql status```
+  * client 
+    * ```mysql -u root -proot```
+* nginx (starts on boot)
+  * serves content from ```/home/vagrant/public_html``` 
+    * [http://localhost:8080](http://localhost:8080)
+    * [https://localhost:4443](https://localhost:4443)
+    * [http://192.168.100.100](http://192.168.100.100)
+    * [https://192.168.100.100](https://192.168.100.100)
+  * management (```Usage: nginx {start|stop|restart|reload|force-reload|status|configtest|rotate|upgrade}```)
+    * ```sudo service nginx stop```
+    * ```sudo service nginx start```
+    * ```sudo service nginx reload```
+    * ```sudo service nginx restart```
+    * ```sudo service nginx status```
+* tomcat 8 (starts on boot) 
+  * http://192.168.100.100:8080 (root content)
+  * http://192.168.100.100/manager (manager - configuration in nginx)
+  * http://192.168.100.100:8080/manager
+  * management (```Run as /etc/init.d/tomcat <start|stop|restart>```)
+    * ```sudo service tomcat stop```
+    * ```sudo service tomcat start```
+    * ```sudo service tomcat restart```
+* git
+* svn
+* mc
+* vim
+* java tools
+  * mvn
+  * ant
+  * gradle
+* environment managers
+  * [jenv](https://github.com/gcuisinier/jenv.git)
+  * [rbenv](https://github.com/sstephenson/rbenv.git)
+  * [nodenv](https://github.com/OiNutter/nodenv.git)
+  * [pyenv](https://github.com/yyuu/pyenv.git)
+* port forwarding 
+  * http ```8080 (host) -> 80 (guest)```
+  * https ```4443 (host) -> 443 (guest)```
+* enabled private network interface (guest -> ```192.168.100.100```)
+* current host directory mounted in guest at ```/vagrant```
+
+## connecting to mysql server
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ mysql -u root -proot
+```
+## how to use jenv
+
+### Configure which jvm to use
+
+*globally*
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ jenv global 1.8
+```
+*locally* (means which jvm to use in the current directory)
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ jenv local 1.7
+```
+*shell instance* (which jvm to use in the current shell instance)
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ jenv shell 1.7
+```
+
+*precedence*
+
+1. shell
+2. local
+3. global
+
+### Configure jvm options
+
+*globally*
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ jenv global-options "-Xmx512m"
+```
+*locally*
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ jenv local-options "-Xmx512m"
+```
+*shell instance*
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ jenv shell-options "-Xmx512m"
+```
+
+*precedence*
+
+same as for 'Configure which jvm to use'
+
+### Configure gc logging in shell scope
+
+To set gc logging on do
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ gc_set
+```
+and
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ jenv info java
+```
+will output
+```
+Jenv will exec : /home/vagrant/.jenv/versions/1.8/bin/java -XX:+PrintGCDetails -Xloggc:gc.log
+Exported variables :
+  JAVA_HOME=/home/vagrant/.jenv/versions/1.8
+```
+
+To unset do
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ gc_unset
+```
+
+### Configure debug options
+
+To set debug options on do
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ jdebug_set
+```
+and
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ jenv info java
+```
+will output
+```
+Jenv will exec : /home/vagrant/.jenv/versions/1.8/bin/java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n
+Exported variables :
+  JAVA_HOME=/home/vagrant/.jenv/versions/1.8
+```
+To unset do
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ jdebug_unset
+```
+
+### Configure jrebel
+
+To use jrebel (```jrebel.jar``` must be placed in ```/home/vagrant/bin/jrebel/```) during starting applications do
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ jrebel_set
+```
+and
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ jenv info java
+```
+will output
+```
+Jenv will exec : /home/vagrant/.jenv/versions/1.8/bin/java -javaagent:/home/vagrant/bin/jrebel/jrebel.jar -noverify
+Exported variables :
+  JAVA_HOME=/home/vagrant/.jenv/versions/1.8
+```
+
+### Configure jprofiler
+
+To profile application with jprofiler (jprofiler ```agent.jar``` must to be placed in ```/home/vagrant/bin/jprofiler/bin/```) do
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ jprofiler_set
+```
+and
+```
+vagrant@vagrant-ubuntu-vivid-64:~$ jenv info java
+```
+will output
+```
+Jenv will exec : /home/vagrant/.jenv/versions/1.8/bin/java -javaagent:/home/vagrant/bin/jprofiler/bin/agent.jar
+Exported variables :
+  JAVA_HOME=/home/vagrant/.jenv/versions/1.8
+```
+
+### Resources
+For more information please visit [jenv](https://github.com/gcuisinier/jenv)
---
+++ provision.sh
@@ -0,0 +1,286 @@
+#!/bin/bash
+
+VAGRANT_DIR=/vagrant
+HOME_DIR=~/
+HOME_SERVERS_DIR=$HOME_DIR/servers
+HOME_PUBLIC_HTML_DIR=$HOME_DIR/public_html
+HOME_BIN_DIR=$HOME_DIR/bin
+
+installPackage()
+{
+  local packages=$*
+  echo "Installing $packages"
+  #sudo apt-get install -y $packages >/dev/null 2>&1
+  sudo apt-get install -y $packages
+}
+
+indent() 
+{
+  echo -n '    '
+}
+
+downloadWithProgress()
+{
+  local url=$2
+  local file=$1
+  echo -n "Downloading $file:"
+  echo -n "    "
+  wget --progress=dot $url 2>&1 | grep --line-buffered "%" | sed -u -e 's/\.//g' | awk '{printf("\b\b\b\b%4s", $2)}'
+  echo -ne "\b\b\b\b"
+  echo " DONE"
+}
+
+download()
+{
+  local url=$2
+  local file=$1
+  echo "Downloading $file"
+  #wget --progress=dot $3 $url >/dev/null 2>&1
+  wget --progress=dot $3 $url
+}
+
+installMysql() 
+{
+  #setting non-interactive mode
+  echo mysql-server mysql-server/root_password password root | sudo debconf-set-selections
+  echo mysql-server mysql-server/root_password_again password root | sudo debconf-set-selections
+  indent; installPackage mysql-server
+  indent; indent; echo 'Creating /etc/mysql/conf.d/utf8_charset.cnf'
+  sudo cp $VAGRANT_DIR/mysql/utf8_charset.cnf /etc/mysql/conf.d/utf8_charset.cnf
+  indent; indent; echo 'Restarting mysql'
+  #sudo service mysql restart >/dev/null 2>&1
+  sudo service mysql restart
+}
+
+installPackages()
+{
+  echo "Installing packages"
+  indent; echo 'apt-get update'
+  sudo apt-get update >/dev/null 2>&1
+  indent; installPackage vim
+  indent; installPackage git
+  indent; installPackage mc
+  indent; installPackage apg
+  installMysql
+}
+
+createDirs()
+{
+  echo 'Creating directories'
+  indent; echo 'Creating bin directory'
+  mkdir $HOME_BIN_DIR
+  indent; echo 'Creating public_html directory'
+  mkdir $HOME_PUBLIC_HTML_DIR
+  chmod o+xr $HOME_PUBLIC_HTML_DIR
+  mkdir $HOME_SERVERS_DIR
+  indent; echo 'Creating servers directory'
+}
+
+downloadJdks()
+{
+  echo "Downloading jdks"
+  
+  #jdk8
+  jdk="jdk-8u112-linux-x64.tar.gz"
+  if [ ! -e $jdk ] 
+  then
+    indent; echo "There is no $jdk"
+    indent; indent; download "$jdk" "http://download.oracle.com/otn-pub/java/jdk/8u112-b15/$jdk" '--no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie"'
+  else
+    indent; echo "$jdk is available"
+  fi
+  
+  
+  #jdk7
+  jdk="jdk-7u79-linux-x64.tar.gz"
+  if [ ! -e $jdk ] 
+  then
+    indent; echo "There is no $jdk"
+    indent; indent; download "$jdk" "http://download.oracle.com/otn-pub/java/jdk/7u79-b15/$jdk" '--no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie"'
+  else
+    indent; echo "$jdk is available"
+  fi
+}
+
+installJdks()
+{
+  echo 'Installing jdks'
+  for file in `ls jdk*.tar.gz`
+  do 
+    indent; echo "Extracting $file"
+    #tar xvzf ./$file >/dev/null 2>&1
+    tar xvzf ./$file
+  done
+  indent; echo 'Cleaning'
+  rm jdk*.tar.gz
+}
+
+installEnvManagers()
+{
+  echo 'Installing environment managers (for Java) '
+  indent; echo 'Installing jenv'
+  indent; indent; echo 'Clonning from github to ~/.jenv'
+  #git clone https://github.com/gcuisinier/jenv.git ~/.jenv >/dev/null 2>&1
+  git clone https://github.com/gcuisinier/jenv.git ~/.jenv
+  indent; indent; echo "Setting environment variables"
+  export PATH="$HOME/.jenv/bin:$PATH"
+  eval "$(jenv init -)"
+  indent; indent; echo 'Make build tools jenv aware'
+  message=`jenv enable-plugin ant`
+  indent; indent; indent; echo $message
+  message=`jenv enable-plugin maven`
+  indent; indent; indent; echo $message
+  message=`jenv enable-plugin gradle`
+  indent; indent; indent; echo $message
+  message=`jenv enable-plugin sbt`
+  indent; indent; indent; echo $message
+}
+
+updateBashrc()
+{
+  echo 'Updating .bashrc'
+  cat $VAGRANT_DIR/bashrc.template >> $HOME_DIR/.bashrc
+  source $HOME_DIR/.bashrc
+}
+
+
+installRuntimes()
+{
+  echo 'Install runtimes using environment managers'
+  indent; echo 'Install java'
+  #for jdk in `ls $HOME_BIN_DIR/ | grep jdk`; do jenv add $HOME_BIN_DIR/$jdk >/dev/null 2>&1; done
+  for jdk in `ls $HOME_BIN_DIR/ | grep jdk`; do jenv add $HOME_BIN_DIR/$jdk; done
+  indent; echo 'Set jdk 1.8 globally'
+  jenv global 1.8
+}
+
+
+installingApp()
+{
+  local tool_name=$1
+  local file=$2
+  local url=$3
+  local link_src=$4
+  local link_target=$5
+  echo "Installing $tool_name"
+  indent; download $file $url
+  indent; echo -n "Extracting $file"
+  if [[ "$file" =~ .*tar.gz$ || "$file" =~ .*tgz$ ]]
+  then 
+    echo " using tar"
+    #tar xvzf $file >/dev/null 2>&1
+    tar xvzf $file 
+  else
+    if [[ "$file" =~ .*zip$ ]]
+    then
+      echo " using unzip"
+      #unzip $file >/dev/null 2>&1
+      unzip $file 
+    else
+      echo
+      indent; indent; echo "Can't extract $file. Unknown ext"
+    fi
+  fi
+  indent; echo 'Cleaning'
+  rm $file
+  indent; echo "Creating symbolic link $link_target"
+  ln -s $link_src $link_target
+}
+
+installingMvn()
+{
+  installingApp 'apache-maven' \
+    apache-maven-3.3.9-bin.tar.gz \
+    http://www.eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz \
+    'apache-maven*' \
+    apache-maven
+}
+
+installingAnt()
+{
+  installingApp 'apache-ant' \
+    apache-ant-1.9.7-bin.tar.gz \
+    http://www.eu.apache.org/dist/ant/binaries/apache-ant-1.9.7-bin.tar.gz \
+    'apache-ant*' \
+    apache-ant
+}
+
+installingGradle()
+{
+  installingApp 'gradle' \
+    gradle-2.8-bin.zip \
+    https://services.gradle.org/distributions/gradle-2.8-bin.zip \
+    'gradle-*' \
+    gradle
+}
+
+installingSbt()
+{
+  installingApp 'sbt' \
+    sbt-0.13.9.tgz \
+    https://dl.bintray.com/sbt/native-packages/sbt/0.13.9/sbt-0.13.9.tgz \
+    'sbt' \
+    sbt
+}
+
+installingTools() 
+{
+  cd $HOME_BIN_DIR
+  installingMvn
+  installingAnt
+  installingGradle
+  installingSbt
+}
+
+installingTomcat()
+{
+  installingApp 'apache-tomcat' \
+    apache-tomcat-8.0.28.tar.gz \
+    http://ftp.piotrkosoft.net/pub/mirrors/ftp.apache.org/tomcat/tomcat-8/v8.0.39/bin/apache-tomcat-8.0.39.tar.gz \
+    'apache-tomcat*' \
+    apache-tomcat
+
+  indent; echo 'Creating apache-tomcat /bin/setenv.sh'
+  echo 'JAVA_HOME=`jenv javahome`' > apache-tomcat/bin/setenv.sh
+  chmod +x apache-tomcat/bin/setenv.sh
+  indent; echo 'Copying tomcat-users.xml to apache-tomcat/conf'
+  cp $VAGRANT_DIR/tomcat/tomcat-users.xml apache-tomcat/conf
+  indent; echo 'Creating /etc/init.d/tomcat script'
+  sudo cp $VAGRANT_DIR/tomcat/tomcat /etc/init.d/
+  sudo chmod 755 /etc/init.d/tomcat
+  sudo update-rc.d tomcat defaults
+  sudo update-rc.d tomcat enable
+  indent; echo 'Starting tomcat'
+  sudo service tomcat start
+}
+
+installingServers()
+{
+  cd $HOME_SERVERS_DIR
+  #installingTomcat
+}
+
+run() {
+  createDirs
+  installPackages
+  cd $VAGRANT_DIR
+  downloadJdks
+  #echo "Copying jdks to $HOME_BIN_DIR" >/dev/null 2>&1
+  echo "Copying jdks to $HOME_BIN_DIR" 
+  cp -r $VAGRANT_DIR/jdk*.tar.gz $HOME_BIN_DIR
+  cd $HOME_BIN_DIR  
+  installJdks
+  installingTools
+  installEnvManagers
+  updateBashrc
+  installRuntimes
+  installingServers
+}
+
+
+if [ ! -f "/var/vagrant_provision" ]; then
+  sudo touch /var/vagrant_provision
+  run
+else
+  echo "Nothing to do"
+fi
